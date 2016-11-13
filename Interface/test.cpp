48b0c2101d6a3757b372acd8c8b1dc8195eb2dfd
demons <837940593@qq.com>
2016-11-13 11:21:59 +0800

    finish eventbuffer

--- Interface/test.cpp
+++ Interface/test.cpp
@@ -22,10 +22,17 @@ void *put(void *unused)
 int main()
 {
     pthread_t p1 = createPthread(get);
-    pthread_t p2 = createPthread(put);
+    /* pthread_t p2 = createPthread(put); */
     pthread_t p3 = createPthread(get);
     
+    char c;
+    for (int i = 0; i < 2; i++)
+    {
+        std::cin >> c;
+        eb.put(Up);
+    }
+    
     waitPthread(p1);
-    waitPthread(p2);
+    /* waitPthread(p2); */
     waitPthread(p3);
 }
