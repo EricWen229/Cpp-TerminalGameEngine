2a93a8d58c493c9bca4a879e4d9f2ab88bf54f88
demons <837940593@qq.com>
2016-11-14 09:44:59 +0800

    EventBuffer past test

--- EventBuffer/TestEventBuffer.cpp
+++ EventBuffer/TestEventBuffer.cpp
@@ -4,3 +4,90 @@ CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(TestEventBuffer, "TestEventBuffer");
 
 TestEventBuffer::TestEventBuffer() {}
 TestEventBuffer::~TestEventBuffer() {}
+
+extern EventBuffer eb;
+
+void *get(void *unused)
+{
+    Event e = eb.get();
+    std::cout << e << std::endl;
+    return null;
+}
+
+void *put(void *unused)
+{
+    /* sleep(1); */
+    eb.put(Up);
+    /* sleep(2); */
+    eb.put(Right);
+    eb.put(Left);
+    return null;
+}
+
+void TestEventBuffer::testA()
+{
+    pthread_t p[] =
+    {
+        createPthread(get),
+        createPthread(get),
+        createPthread(get),
+        createPthread(put)
+    };
+    
+    for (int i = 0; i < 4; i++)
+    {
+        waitPthread(p[i]);
+    }
+}
+
+void TestEventBuffer::testB()
+{
+    pthread_t p[] =
+    {
+        createPthread(get),
+        createPthread(get),
+        createPthread(put),
+        createPthread(get)
+    };
+    
+    for (int i = 0; i < 4; i++)
+    {
+        waitPthread(p[i]);
+    }
+}
+
+void TestEventBuffer::testC()
+{
+    pthread_t p[] =
+    {
+        createPthread(put),
+        createPthread(get),
+        createPthread(get),
+        createPthread(get)
+    };
+    
+    for (int i = 0; i < 4; i++)
+    {
+        waitPthread(p[i]);
+    }
+}
+
+void TestEventBuffer::testD()
+{
+    pthread_t p[] =
+    {
+        createPthread(put),
+        createPthread(put),
+        createPthread(get),
+        createPthread(get),
+        createPthread(get),
+        createPthread(get),
+        createPthread(get),
+        createPthread(get)
+    };
+    
+    for (int i = 0; i < 8; i++)
+    {
+        waitPthread(p[i]);
+    }
+}
--- EventBuffer/TestEventBuffer.h
+++ EventBuffer/TestEventBuffer.h
@@ -1,14 +1,23 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include "EventBuffer.h"
 
+EventBuffer eb;
+
 class TestEventBuffer: public CppUnit::TestFixture
 {
     private:
         CPPUNIT_TEST_SUITE(TestEventBuffer);
-        /* CPPUNIT_TEST(testNull); */
+        CPPUNIT_TEST(testA);
+        CPPUNIT_TEST(testB);
+        CPPUNIT_TEST(testC);
+        CPPUNIT_TEST(testD);
         CPPUNIT_TEST_SUITE_END();
         
     public:
         TestEventBuffer();
         ~TestEventBuffer();
+        void testA();
+        void testB();
+        void testC();
+        void testD();
 };
