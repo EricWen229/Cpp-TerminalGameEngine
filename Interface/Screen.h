217c9e3d534edbcee9eed82571bb9caa682d2137
demons <837940593@qq.com>
2016-12-15 20:19:24 +0800

    .

--- Interface/Screen.h
+++ Interface/Screen.h
@@ -2,20 +2,37 @@
 #define Screen_H
 
 #include "../Array/Array.h"
+#include "Interface.h"
+#include "NoticeBoard.h"
+#include <vector>
 #include <ncurses.h>
+
+typedef int Id;
+
+/* manage book */
+struct ManBook
+{
+    int left;
+    int top;
+    int width;
+    int height;
+};
 
 template <class T>
 class Screen
 {
     private:
         static int height, width;
-        static bool firstInstance;
         static SmartArray<T> buffer;
         static SmartArray<bool> allocated;
+        static std::vector<ManBook> manBooks;
         
     public:
         Screen(int h, int w);
         ~Screen();
+        
+        Interface<T> &getInterface();
+        NoticeBoard<T> &getNoticBoard(const Id &id);
 };
 
 template <class T>
@@ -23,9 +40,6 @@ int Screen<T>::height;
 
 template <class T>
 int Screen<T>::width;
-
-template <class T>
-bool Screen<T>::firstInstance = true;
 
 template <class T>
 SmartArray<T> Screen<T>::buffer;
@@ -36,31 +50,23 @@ SmartArray<bool> Screen<T>::allocated;
 template <class T>
 Screen<T>::Screen(int h, int w)
 {
-    if (firstInstance)
+    height = h;
+    width = w;
+    
+    buffer = createArray<T>(height, width);
+    allocated = createArray<T>(height, width);
+    
+    for (int i = 0; i < height; i++)
     {
-        height = h;
-        width = w;
-        
-        buffer = createArray<T>(height, width);
-        allocated = createArray<T>(height, width);
-        
-        for (int i = 0; i < height; i++)
+        for (int j = 0; j < width; j++)
         {
-            for (int j = 0; j < width; j++)
-            {
-                allocated[i][j] = false;
-            }
+            allocated[i][j] = false;
         }
-        
-        firstInstance = false;
     }
 }
 
 template <class T>
-Screen<T>::~Screen()
-{
-    firstInstance = true;
-}
+Screen<T>::~Screen() {}
 
 class ScreenChar: public Screen<char>
 {
